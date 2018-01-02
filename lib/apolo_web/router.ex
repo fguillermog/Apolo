99bd605348e141a5a72f21bb9eee732126320fcd
Erik Vera <apcxiii@gmail.com>
2018-01-02 11:39:44 -0600

    secret html

--- lib/apolo_web/router.ex
+++ lib/apolo_web/router.ex
@@ -13,10 +13,28 @@ defmodule ApoloWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :auth do
+    plug Apolo.Auth.Pipeline
+  end
+
+  pipeline :ensure_auth do
+    plug Guardian.Plug.EnsureAuthenticated    
+  end
+
+  #Maybe logged in scoped
   scope "/", ApoloWeb do
-    pipe_through :browser # Use the default browser stack
+    pipe_through [:browser, :auth] # Use the default browser stack
 
     get "/", PageController, :index
+    post "/", PageController, :login
+    post "/logout", PageController, :logout
+  end
+
+
+  #Definitely logged in scope
+  scope "/", ApoloWeb do
+    pipe_through [:browser, :auth, :ensure_auth]
+    get "secret", PageController, :secret
   end
 
   # Other scopes may use custom stacks.
