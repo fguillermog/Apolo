4bfe2e1f8cbd62bb1333cf12d1de3ccd5cb4621c
Erik Vera <apcxiii@gmail.com>
2018-01-02 12:24:19 -0600

    added seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,5 +10,5 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-AuthEx.Auth.create_user(%{username: "fguillermo", password: "Apolo2017!"})
-AuthEx.Auth.create_user(%{username: "evera", password: "Apolo2017!"})
+Apolo.Auth.create_user(%{username: "fguillermo", password: "Apolo2017!"})
+Apolo.Auth.create_user(%{username: "evera", password: "Apolo2017!"})
