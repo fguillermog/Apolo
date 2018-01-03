cd04877449c3bd3f9ea063c7f8e0dadfa12b2e5c
Erik Vera <apcxiii@gmail.com>
2018-01-02 18:05:03 -0600

    format and added /secret

--- config/config.exs
+++ config/config.exs
@@ -24,7 +24,7 @@ config :logger, :console,
 
 config :apolo, Apolo.Auth.Guardian,
   issuer: "apolo", # Name of your app/company/product
-  secret_key: System.get_env("GUARDIAN_SECRET_KEY") # Replace this with the output of the mix command
+  secret_key: "foobar" # Replace this with the output of the mix command
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
