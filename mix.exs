4d4b23d69fd9a3f961fb49971960f8b07a440466
Erik Vera <apcxiii@gmail.com>
2018-01-28 12:50:50 -0600

    added elixir 1.5 version and version 0.0.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Apolo.Mixfile do
     [
       app: :apolo,
       version: "0.0.3",
-      elixir: "~> 1.6",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
